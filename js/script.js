68c3cf8fafe73c77ec0678a4c0716f4ab6deb805
kamrujs <source.akash@gmail.com>
2021-05-05 21:16:58 +0600

    beatles frisht

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.venobox').venobox({
+    border     : '3px',
+    bgcolor    : '#fff',
+    titleattr  : 'data-title',
+    numeratio  : true,
+    infinigall : true,
+    share      : ['facebook', 'twitter', 'download'] ,
+    spinner : 'cube-grid',
+}); 
+});
+
+
